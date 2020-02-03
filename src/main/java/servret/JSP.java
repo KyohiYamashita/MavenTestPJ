6ef3af94a5ca56d432047a3b5a41a036552a2292
Kyohei Yamashita <kyohei.yamashita@claudio.co.jp>
2020-02-03 15:17:32 +0900

    Maven01

---
+++ src/main/java/servret/JSP.java
@@ -0,0 +1,33 @@
+package servret;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import work.WorkJava;
+
+@WebServlet("/cbwork")
+public class JSP extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+		throws ServletException, IOException {
+	
+		    request.setCharacterEncoding("Shift_JIS");
+		
+		    String First = request.getParameter("first");
+//		    String Second = request.getParameter("second");
+		    
+		    WorkJava cl = new WorkJava();
+		    	
+//    		CobolCalculator cobolCalculator = new CobolCalculator();
+     		String data = cl.ConnectDB(First);
+//    		String data = Integer.toString(cobolCalculator.add(Integer.parseInt(First), Integer.parseInt(Second)));    		 
+//    		String data = First + "to" + Second;
+//			response.getWriter().write(First + "+" +Second + "=");
+    		response.getWriter().write(data);
+		}
+
+}
---
+++ src/main/java/work/WorkJava.java
@@ -0,0 +1,63 @@
+package work;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WorkJava {
+	public String ConnectDB(String first) {
+		
+	    String data = Integer.toString(0);
+	    
+	    // DB
+	    String driver = "JP.co.Hitachi.soft.HiRDB.JDBC.HiRDBDriver";
+        try {
+            Class.forName(driver);
+        }catch(Exception e){
+        	e.printStackTrace();
+        	return "0";
+        } 
+ 
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        
+        System.setProperty("jdbc.drivers", "JP.co.Hitachi.soft.HiRDB.JDBC.HiRDBDriver");
+        
+        try {
+            // 
+        	con = DriverManager.getConnection("jdbc:hitachi:hirdb://DBID=22200,DBHOST=hirdbserver,USER=USER1,PASSWORD=USER1");
+            
+            pstmt = con.prepareStatement("SELECT ITEM FROM CONNTEST WHERE ITEMNO = " + first);
+		    // ResultSet取得
+		    rs = pstmt.executeQuery();
+            
+		    while(rs.next()) {
+				System.out.print(rs.getString("ITEM"));
+				data = rs.getString("ITEM");
+		    }
+		    // ResultSetの解放
+			rs.close();
+			// PreparedStatementの解放
+			pstmt.close();
+			// 
+			con.close();
+			
+			return data;
+			
+        }catch(SQLException e){
+			//
+			e.printStackTrace();
+			// SQLSTATEの出力
+			System.out.println("SQLSTATE=" + e.getSQLState());
+			// SQLCODEの出力
+			System.out.println("SQLCODE=" + e.getErrorCode());
+			// SQLERRM(SQLメッセージ)の出力
+			System.out.println("SQLERRM=" + e.getMessage());
+			
+			return "9";
+        }
+	}
+}
